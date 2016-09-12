830dc6a0d3567e0c6bc92c12de3cbf9998ef7c12
Nathan Fulton <nathanfulton104@gmail.com>
2016-09-12 10:09:06 -0400

    Correct use of Android system UI flags
    Removed the use of SYSTEM_UI_FLAG_HIDE_NAVIGATION and
    SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION for API 14 through API 18.

--- game/sdl/android/src/com/spiffcode/ht/GameActivity.java
+++ game/sdl/android/src/com/spiffcode/ht/GameActivity.java
@@ -84,30 +84,23 @@ public class GameActivity extends SDLActivity {
 
 	@SuppressLint("NewApi")
 	private static int getVisibilityOptions() {
-		// Minimum api for this block to safely run is 11
-		if (android.os.Build.VERSION.SDK_INT >= 11) {
-			int options = 0;
-			
-			if (android.os.Build.VERSION.SDK_INT >= 14) {
-				options = options | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-			}
-			
-			if (android.os.Build.VERSION.SDK_INT >= 16) {
-				options = options |
-					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_FULLSCREEN;
-			}
-			
-			if (android.os.Build.VERSION.SDK_INT >= 19) {
-				options = options | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-			}
-	
-			return options;
+
+		int options = 0;
+		
+		if (android.os.Build.VERSION.SDK_INT >= 16) {
+			options = options |
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_FULLSCREEN;
 		}
 		
-		// Return 0 if under api 11
-		return 0;
+		if (android.os.Build.VERSION.SDK_INT >= 19) {
+			options = options | 
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+		}
+	
+		return options;
 	}
 }
