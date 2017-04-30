8cb3f7101479591952169a35ff44805945fc3b3a
Nathan Fulton <nathanfulton104@gmail.com>
2017-04-30 00:23:13 -0400

    data/makedirs.sh add htdata2432

--- data/makedirs.sh
+++ data/makedirs.sh
@@ -1,4 +1,5 @@
 mkdir 824 2> /dev/null > /dev/null && false
 mkdir 832 2> /dev/null > /dev/null && false
+mkdir htdata2432 2> /dev/null > /dev/null && false
 mkdir trg 2> /dev/null > /dev/null && false
 exit 0
